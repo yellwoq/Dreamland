1839e3642ff06193c39b1791b5588678e414b01a
yellwoq <1551935747@qq.com>
2023-06-19 00:40:55 +0800

    Unity项目初次提交

---
+++ Dreamland/Assets/Scripts/UI/GamePanel.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour
+{
+    private Button btn_Pause;
+    private Button btn_Play;
+    private Text txt_Score;
+    private Text txt_DiamondCount;
+
+    private void Awake()
+    {
+        EventCenter.AddListener(EventDefine.ShowGamePanel, Show);
+        Init();
+    }
+    private void Init()
+    {
+        btn_Pause = transform.Find("btn_Pause").GetComponent<Button>();
+        btn_Pause.onClick.AddListener(OnPauseButtonClick);
+        btn_Play = transform.Find("btn_Play").GetComponent<Button>();
+        btn_Play.onClick.AddListener(OnPlayButtonClick);
+        txt_Score = transform.Find("txt_Score").GetComponent<Text>();
+        txt_DiamondCount = transform.Find("Diamond/txt_DiamondCount").GetComponent<Text>();
+        btn_Play.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+    private void OnDestroy()
+    {
+        EventCenter.RemoveListener(EventDefine.ShowGamePanel, Show);
+    }
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    /// <summary>
+    /// 更新成绩显示
+    /// </summary>
+    /// <param name="score"></param>
+    private void UpdateScoreText(int score)
+    {
+        txt_Score.text = score.ToString();
+    }
+    /// <summary>
+    /// 更新钻石数量显示
+    /// </summary>
+    /// <param name="diamond"></param>
+    private void UpdateDiamondText(int diamond)
+    {
+        txt_DiamondCount.text = diamond.ToString();
+    }
+    /// <summary>
+    /// 暂停按钮点击
+    /// </summary>
+    private void OnPauseButtonClick()
+    {
+        btn_Pause.gameObject.SetActive(false);
+        btn_Play.gameObject.SetActive(true);
+    }
+    /// <summary>
+    /// 开始按钮点击
+    /// </summary>
+    private void OnPlayButtonClick()
+    {
+        btn_Pause.gameObject.SetActive(true);
+        btn_Play.gameObject.SetActive(false);
+    }
+}
